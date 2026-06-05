a1d48391fde8c2303cd07893d5bfd49dd67922c0
bichngoc21 <duongthibichngoc0527@gmail.com>
2026-06-05 14:59:33 +0700

    add css

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,12 @@
-/*dinh dang*/
+function printBCC(num)
+{
+    let result = "";
+
+    let i = 1;
+    while(i<=10)
+    {
+        result+= `${num} x ${i} = ${num * i} <br>`;
+        i++;
+    }
+    document.getElementById("result"). innerHTML = result;
+}
